570d25354f8dbc34f7d53e620db7484b319d6557
HuangSiman <sh894@mail.missouri.edu>
2015-10-18 15:37:29 -0500

    admin_login

---
+++ Admin/login.php
@@ -0,0 +1,32 @@
+<?php
+
+	require_once "db_func.php";
+	session_start();
+	$name = $_POST['user'];
+	$password = $_POST['password'];
+
+	$_SESSION["logintime"] = NULL;
+	$con = DBconnect();
+	if(isset($_POST['user'])&&$_POST['user']!=null){
+		if(isset($_POST['password'])&&$_POST['password']!=null){
+			adminlogin($con,$_POST['user'],$_POST['password']);
+		}
+	}
+
+	function adminlogin($con,$user,$paswd){
+		$admin_index_url = 'http://localhost:8888/Admin/admin_index.html';
+		$sql_paswd = "SELECT password FROM Admin WHERE username = '$user'" ;
+		$result_paswd = mysql_query($sql_paswd,$con);
+
+		$password = mysql_fetch_assoc($result_paswd);
+		//print_r(mysql_fetch_assoc($result_pasws));
+		var_dump($password);
+//		var_dump($paswd);
+		if ((string)$paswd==$password['password']){
+			header('Location:www.google.com' );
+			//header('Location:' .$admin_index_url);
+			echo "Login Success";
+		}
+		else echo "Invalid password";
+		mysql_close($con);
+	}
