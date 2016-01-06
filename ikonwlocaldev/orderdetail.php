648db45e26e02132b9d0efdf83e1a5b4e4a141be
YuduDu <dyd12315@163.com>
2016-01-06 03:19:30 -0600

    添加服务时间详情

--- ikonwlocaldev/orderdetail.php
+++ ikonwlocaldev/orderdetail.php
@@ -32,17 +32,33 @@ if (isset($_POST["information"]) && $_POST["information"] != null) {
     //var_dump($order);
     if ($order["customerid"] == $info["customerid"]) {
         $pic = DBfetchone2($con, "Shop", array("pic"), array("shopid" => $order['shopid']));
-        $order = array_merge($order, $pic);
+        $time = DBfetchone2($con,"massagist_appt",array("service_start","service_end"),["orderid"=>$order["orderid"]]);
+        if($pic!=null && $time!=null)
+        {
+            $order = array_merge($order, $pic);
+            $order = array_merge($order, $time);
+            echo json_encode([
+                'RespCode' => '111111',
+                'RespContent' => [
+                    'Status' => 'Success',
+                    'Content' => $order
+                ]
+            ]);
+            $log->addInfo("User with ip " . $_SERVER["REMOTE_ADDR"] . " of id " . $info["customerid"] . " get information of order " . $info["orderid"] . ".");
+
+        }
+        else{
+            echo json_encode([
+                'RespCode' => '000004',
+                'RespContent' => [
+                    'Status' => 'Wrong',
+                    'Content' => "Fail to get shop picture and appointment information, make sure order is correct completed."
+                ]
+            ]);
+            $log->addAlert("!!! User with ip " . $_SERVER["REMOTE_ADDR"] . " of id " . $info["customerid"] . " tried to get order " . $info["orderid"] . "'s information, but information is incomplete");
+            }
         // echo json_encode($order);
-        echo json_encode([
-            'RespCode' => '111111',
-            'RespContent' => [
-                'Status' => 'Success',
-                'Content' => $order
-            ]
-        ]);
-        $log->addInfo("User with ip " . $_SERVER["REMOTE_ADDR"] . " of id " . $info["customerid"] . " get information of order " . $info["orderid"] . ".");
-    } else {
+        } else {
         // echo "orderdetail Error: wrong customerid";
         echo json_encode([
             'RespCode' => '000002',
